7ab561beb0ed12d8cc7072dd05c2eeb3cfb956f7
Anderson Grefa <65833915+mrmango1@users.noreply.github.com>
2022-06-30 16:18:13 +0000

    test

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -45,7 +45,7 @@ const Home = () => {
             <span className={`${letterClass} _14`}>'m</span>
             <img
               src={LogoTitle}
-              alt="JavaScript Developer Name, Web Developer Name"
+              alt="Python Developer Name, Web Developer Name"
             />
             <AnimatedLetters
               letterClass={letterClass}
@@ -59,7 +59,7 @@ const Home = () => {
               idx={22}
             />
           </h1>
-          <h2>Full Stack Developer / Python Expert / Django</h2>
+          <h2>Back End Developer / Python Expert / Django</h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
